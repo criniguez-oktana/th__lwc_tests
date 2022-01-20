ad8d674d1b30aa89f28e380ecc2af61bf55358e7
Cristian Iniguez <cristian.iniguez@oktana.com>
2022-01-20 15:37:43 -0400

    mock other components

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,10 @@ module.exports = {
   ...jestConfig,
   modulePathIgnorePatterns: ["<rootDir>/.localdevserver"],
   moduleNameMapper: {
-    "^lightning/button$": "<rootDir>/force-app/test/jest-mocks/lightning/button"
+    "^lightning/button$":
+      "<rootDir>/force-app/test/jest-mocks/lightning/button",
+    "^thunder/hammerButton$":
+      "<rootDir>/force-app/test/jest-mocks/thunder/hammerButton",
+    "^c/displayPanel$": "<rootDir>/force-app/test/jest-mocks/c/displayPanel"
   }
 };
